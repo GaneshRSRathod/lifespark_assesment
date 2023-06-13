91a72208e00549d946afba4d2c196c541281ac2e
Ganesh Rathod <50762810+GaneshRSRathod@users.noreply.github.com>
2023-06-13 12:18:37 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,2 +1,16 @@
 //Write a C/C++ program that records samples from your computer microphone at 58 kHz sampling rate for 4.5 seconds.
 //Plot the recorded sound
+#include<stdio.h>
+#include<stdlib.h>
+int main() {
+    FILE *file;
+    char c;
+    file = fopen("/etc/alsa/audioFile0.raw", "r");
+    while(fscanf(file, "%c", &c) != -1) {
+               printf("%c", c);
+    }
+    return 0;
+    fclose(file);
+}
+               
+  
